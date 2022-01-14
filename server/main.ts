2c1c8f2f44b8b8ae7406d8e7c3db6756f00c9bff
wjq990112 <1163585385@qq.com>
2022-01-14 20:35:33 +0800

    chore: import type of express

--- server/main.ts
+++ server/main.ts
@@ -1,8 +1,10 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
+import type { NestExpressApplication } from '@nestjs/platform-express';
+
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
   await app.listen(3000);
 }
 bootstrap();
