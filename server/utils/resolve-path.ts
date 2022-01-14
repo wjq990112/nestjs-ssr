7799b53c2061eb4564e5739eb7ebb63c054fa310
wjq990112 <1163585385@qq.com>
2022-01-15 05:19:14 +0800

    chore: add comments

--- server/utils/resolve-path.ts
+++ server/utils/resolve-path.ts
@@ -1,4 +1,9 @@
 import { resolve } from 'path';
 
+/**
+ * resolve client file path
+ * @param pathSegments relative path of file in client
+ * @returns absolute path of file
+ */
 export const resolveClientPath = (...pathSegments: string[]) =>
   resolve(__dirname, '..', '..', 'client', ...pathSegments);
